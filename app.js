bcfef8854f74c5a9a5e8c8fd36e60628eb997b04
mhuzaifa0007 <159468825+mhuzaifa0007@users.noreply.github.com>
2025-10-20 18:24:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-    // Problem 1
+// Problem 1
 
-var firstName = "Hamza Ahmed";
+var firstName = "Huzaifa Ahmed";
 var lastName = "Shaikh";
 var address = "Karachi";
 
@@ -12,50 +12,45 @@ document.writeln(
   "Address ==> " + address
 );
 
-    // Problem 2
+// Problem 2
 
+var street = "Buffer Zone 15A4";    
+var country = "Pakistan"; 
+var fullAddress = "Pakistan"; 
 
-var street = "Buffer Zone 15A4"    
-var country = "Pakistan" 
-var fullAddress = "Pakistan" 
+fullAddress = country + " " + street;
 
+console.log("full mailing address ==>" + " " + fullAddress);
 
-fullAddress = country +" "+ street
+// Problem 3
 
-console.log("full mailing address ==>" +" "+ fullAddress)
+var plate1 = 2;
+var plate2 = 10;
+var plate3 = 4;
 
+var totalPlates = plate1 + plate2 + plate3;
 
-    // Problem 3
+var people = 7;
 
-var plate1 = 2
-var plate2 = 10
-var plate3 = 4
+var remainingPlates = totalPlates - people;
 
-var totalPlates = plate1 + plate2 + plate3
+alert("There are " + remainingPlates + " plates available");
 
+// Problem 4
 
-var people = 7
+var fruits = ["Strawberries", "Mango", "Peach", 69];
 
-remainingPlates = totalPlates - people
+var first = fruits[1];
+fruits[fruits.length - 1] = "Huzaifa Bhai";
 
-alert ("There are" +" "+ remainingPlates +" "+ "plates available")
+console.log(first);
+console.log(fruits);
 
-    // Problem 4
+// Problem 5
 
+function calculateArea(width, height) {
+  var Area = width * height;
+  console.log(Area);
+}
 
-var fruits = ["Strawberries", "Mango", "Peach", 69]
-
-var first = fruits[1]
-fruits [fruits.length -1 ] = "Huzaifa Bhai"
-
-console.log (first)
-console.log (fruits)
-
-    // Problem 5
-
-function calculateArea(width, height)  {
-  Area = width * height
-  console.log(Area)
-}  
-
-calculateArea(5 , 20)
+calculateArea(5, 20);
